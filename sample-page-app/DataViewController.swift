c4f4d12c7b3107dc8bb05f1519bef429714e492f
super-fishz <super_fishz@core-a.org>
2019-02-21 17:57:28 +0900

    init project

---
+++ sample-page-app/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  sample-page-app
+//
+//  Created by jazz on 21/02/2019.
+//  Copyright © 2019 mars. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ sample-page-appTests/sample_page_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample_page_appTests.swift
+//  sample-page-appTests
+//
+//  Created by jazz on 21/02/2019.
+//  Copyright © 2019 mars. All rights reserved.
+//
+
+import XCTest
+@testable import sample_page_app
+
+class sample_page_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
